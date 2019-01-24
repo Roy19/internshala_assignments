7846638b3a68817b7274c563d0e173980ce040e5
Aritra Sur Roy <aritra1997@hotmail.com>
2019-01-24 12:29:22 +0530

    Completed SQL chapter

---
+++ sql/create.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE store;
+
+CREATE TABLE items(
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    price INT(11) NOT NULL);
+
+CREATE TABLE users(
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    contact VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL);
+
+CREATE TABLE users_items(
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    user_id INT(11) NOT NULL,
+    item_id INT(11) NOT NULL,
+    status enum('Added to cart', 'Confirmed') NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY (item_id) REFERENCES items(id));
+
---
+++ sql/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO items(id, name, price)
+VALUES
+    (1, 'Canon EOS',36000),
+    (2, 'Nikon DSLR',40000),
+    (3, 'Sony DSLR', 45000),
+    (4, 'Olympus DSLR', 50000),
+    (5, 'Titan Model #301', 13000),
+    (6, 'Titan Model #201', 3000),
+    (7, 'HMT Milan', 8000),
+    (8, 'Faber Luba #111', 18000),
+    (9, 'H&W', 800),
+    (10, 'Luis Phil', 1000),
+    (11, 'John Zok', 1500),
+    (12, 'Jhalsani', 1300);
